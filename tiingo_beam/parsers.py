9b4935f015e432b03e88f352fbf9a6853a8c290d
Patrick Selamy <pselamy@gmail.com>
2023-05-06 18:14:35 -0400

    Create parsers.py

---
+++ tiingo_beam/parsers.py
@@ -0,0 +1,43 @@
+import dataclasses
+import dateutil.parser
+import json
+import logging
+from typing import Optional, Protocol, Text
+
+from tiingo_beam import models
+
+
+@dataclasses.dataclass(frozen=True)
+class TradeParser(Protocol):
+    def parse(self, message: Text) -> Optional[models.Trade]:
+        ...
+
+
+class CryptoTradeParser(TradeParser):
+    def parse(self, message: Text) -> Optional[models.Trade]:
+        try:
+            # Pull records from an external service.
+            record = json.loads(message)
+            data = record.get("data", [])
+            if data[0] != "T" or len(data) != 6:
+                return
+
+            symbol = data[1].upper()
+            return models.Trade(
+                exchange_name=data[3],
+                instrument=models.Instrument(
+                    symbol=symbol,
+                    type=models.InstrumentType.CRYPTO,
+                ),
+                volume=float(data[4]),
+                price=float(data[5]),
+                time=dateutil.parser.isoparse(data[2]),
+            )
+        except (
+            json.decoder.JSONDecodeError,
+            AttributeError,
+            IndexError,
+            TypeError,
+            ValueError,
+        ):
+            logging.error("Unable to parse trade from %s", message)
